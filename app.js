0da564224fecde976c08626b8e2a36189d2ec5a6
Yuxin Ye <yuxin_ye@protonmail.com>
2022-07-04 12:47:00 +1000

    add engagement data

--- app.js
+++ app.js
@@ -507,3 +507,86 @@ d3.csv("./data/tactical_fix_data.csv").then(function (data) {
       return color(d.value);
     });
 });
+
+// Engagement
+const svg_g = d3
+  .select("#engagement")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", `translate(${margin.left},${margin.top})`);
+
+svg_g
+  .append("text")
+  .attr("x", width / 2)
+  .attr("y", 2 - margin.top / 2)
+  .attr("text-anchor", "middle")
+  .style("font-size", "16px")
+  .style("text-decoration", "underline")
+  .text("Engagement");
+
+// read data
+d3.csv("./data/engagement_data.csv").then(function (data) {
+  // Add X axis
+  const x = d3
+    .scaleLinear()
+    .domain([0, 10])
+    .range([margin.left, width - margin.right]);
+  svg_g
+    .append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x));
+  svg_g
+    .append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "end")
+    .attr("x", width)
+    .attr("y", height - 6)
+    .text("Importance");
+
+  // Add Y axis
+  const y = d3
+    .scaleLinear()
+    .domain([0, 10])
+    .range([height - margin.bottom, margin.top]);
+  svg_g.append("g").call(d3.axisLeft(y));
+  svg_g
+    .append("text")
+    .attr("class", "y label")
+    .attr("text-anchor", "end")
+    .attr("y", 6)
+    .attr("dy", ".75em")
+    .attr("transform", "rotate(-90)")
+    .text("Satisfaction");
+
+  // Prepare a color palette
+  const color = d3
+    .scaleLinear()
+    .domain([0, 0.001]) // Points per square pixel.
+    .range(["white", brandColorC]);
+
+  // compute the density data
+  const densityData = d3
+    .contourDensity()
+    .x(function (d) {
+      return x(d.x);
+    })
+    .y(function (d) {
+      return y(d.y);
+    })
+    .size([width, height])
+    .bandwidth(20)(data);
+
+  // show the shape
+  svg_g
+    .insert("g", "g")
+    .selectAll("path")
+    .data(densityData)
+    .enter()
+    .append("path")
+    .attr("d", d3.geoPath())
+    .attr("fill", function (d) {
+      return color(d.value);
+    });
+});
